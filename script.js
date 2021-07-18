b5c1576fbdf5ec1d1b7174def68226575e9a4945
Nikolaevmar <marioniko001@gmail.com>
2021-07-18 14:31:56 +0300

    Add mainly styling

---
+++ script.js
@@ -0,0 +1,14 @@
+// const gamepad = document.querySelector(`.gamepad`);
+// const buttons = document.querySelector(`.buttons`);
+// const colorChoice = document.getElementById(`colorChoice`);
+// const resetChoice = document.getElementById(`resetChoice`);
+
+// function creatDivs(col , rows) {
+//     for(let i = 0;i < (col * rows); i++) {
+//         const div = document.createElement('div') 
+//         gamepad.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+//         gamepad.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+//         gamepad.appendChild(div).classList.add('box')
+//     }
+// }
+// creatDivs(16,16)
